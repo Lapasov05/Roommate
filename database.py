97a84437b56513efc200104c3e4a398d35ad074d
Nizom Qarshiyev <nizomiddinqarshiyev>
2024-04-16 16:24:52 +0500

    project created

---
+++ database.py
@@ -0,0 +1,16 @@
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_PORT, DB_HOST
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from typing import AsyncGenerator
+
+DB_URL = f"postgres+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_async_engine(DB_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=True)
+Base = declarative_base()
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
